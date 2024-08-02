ea7ea69335f9005c4c7362f631be5dc0e5528bfc
Hogra <hogracubu@gmail.com>
2024-08-02 10:02:25 +0300

    исправил репорты и админа

--- admintool.py
+++ admintool.py
@@ -101,6 +101,7 @@ async def search(update, context):
 
 
 
+
 async def admin(update, context):
     await update.message.reply_text('Конечно я могу выдать роль админа. Дайте мне id этоо счастливчка')
     return 1
